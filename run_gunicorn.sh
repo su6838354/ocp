de5402302c5526f836421a0d181589881dde18fb
root <root@iZ11hl63ehqZ.(none)>
2017-02-19 12:39:27 +0800

    run_gunicorn

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,2 +1,3 @@
 #！/bin/bash
-nohup /data/suyuan/bin/gunicorn ocp.wsgi:application -k gevent -b 0.0.0.0:3390 --reload &
+source /data/suyuan/ocp_env/bin/activate
+nohup gunicorn ocp.wsgi:application -k gevent -b 0.0.0.0:3391 --reload &
